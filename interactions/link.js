ae169905b3cce8cc6de857221cb76a37fa3d9f5c
Kaluub <kaluubs@gmail.com>
2022-11-15 16:07:49 -0500

    testing: login feature

---
+++ interactions/link.js
@@ -0,0 +1,42 @@
+import DefaultInteraction from "../defaultInteraction.js";
+import { ActionRowBuilder, InteractionType, ModalBuilder, SlashCommandBuilder, TextInputBuilder, TextInputStyle } from "discord.js";
+
+class LinkInteraction extends DefaultInteraction {
+    static name = "link";
+    static applicationCommand = new SlashCommandBuilder()
+        .setName(LinkInteraction.name)
+        .setDescription("Link to your Evades.io account")
+
+    constructor() {
+        super(LinkInteraction.name, [InteractionType.ApplicationCommand, InteractionType.MessageComponent]);
+    }
+
+    async execute(interaction) {
+        const loginModal = new ModalBuilder()
+            .setCustomId("login-modal")
+            .setTitle("Evades Login")
+            .addComponents(
+                new ActionRowBuilder()
+                    .addComponents(
+                        new TextInputBuilder()
+                            .setCustomId("username")
+                            .setLabel("Username:")
+                            .setPlaceholder("Your evades username")
+                            .setStyle(TextInputStyle.Short)
+                            .setRequired(true)
+                    ),
+                new ActionRowBuilder()
+                    .addComponents(
+                        new TextInputBuilder()
+                            .setCustomId("password")
+                            .setLabel("Password:")
+                            .setPlaceholder("Your evades password")
+                            .setStyle(TextInputStyle.Short)
+                            .setRequired(true)
+                    )
+            )
+        interaction.showModal(loginModal);
+    }
+}
+
+export default LinkInteraction;
