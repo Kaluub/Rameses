9c9bbe50df2ddc7bd411aa6ace6e554c3f40f2b3
Kaluub <kaluubs@gmail.com>
2022-11-15 17:28:45 -0500

    debug tools

--- interactions/debug.js
+++ interactions/debug.js
@@ -24,6 +24,28 @@ class DebugInteraction extends DefaultInteraction {
                 .setName("checkdb")
                 .setDescription("Count the elements in the database & the amount cached")
         )
+        .addSubcommand(
+            new SlashCommandSubcommandBuilder()
+                .setName("fixusername")
+                .setDescription("Fix (reset) the display name for a player")
+                .addStringOption(
+                    new SlashCommandStringOption()
+                        .setName("username")
+                        .setDescription("The username to fix")
+                        .setRequired(true)
+                )
+        )
+        .addSubcommand(
+            new SlashCommandSubcommandBuilder()
+                .setName("cleardiscord")
+                .setDescription("Remove in-game link for a user's Discord ID")
+                .addStringOption(
+                    new SlashCommandStringOption()
+                        .setName("id")
+                        .setDescription("The user ID to unlink")
+                        .setRequired(true)
+                )
+        )
 
     constructor() {
         super(DebugInteraction.name, [InteractionType.ApplicationCommand]);
@@ -41,7 +63,26 @@ class DebugInteraction extends DefaultInteraction {
         if(subcommand == "checkdb") {
             return `Database stats:\nStored users: ${await AccountData.count()}\nCached users: ${AccountData.cache.size}`;
         }
-        return "How did we get here?"
+        if(subcommand == "fixusername") {
+            const username = interaction.options.getString("username", false);
+            if(!username) return "No user found.";
+            let account = await AccountData.getByUsername(username, false);
+            if(!account) return "No data stored for this user yet.";
+            account.displayName = null;
+            await account.save();
+            return `Display name cleared from "${account?.displayName ?? account.username}"`;
+        }
+        
+        if(subcommand == "cleardiscord") {
+            const userId = interaction.options.getString("id", false);
+            if(!userId) return "User ID not supplied.";
+            let account = await AccountData.getByDiscordId(userId);
+            if(!account) return "No accounts were linked with this user ID.";
+            account.discordId = null;
+            await account.save();
+            return `Discord unlinked from "${account?.displayName ?? account.username}"`;
+        }
+        return "How did we get here?";
     }
 }
 
