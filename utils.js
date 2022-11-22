4e791ce65cf8d8265a07eccb7680ae1b91c77619
Kaluub <kaluubs@gmail.com>
2022-11-22 16:48:13 -0500

    sorting fix

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 function tournamentSorter(run1, run2) {
-    if(run1.area > run2.area) return -1;
+    if(parseInt(run1.area.split(" ")[1]) > parseInt(run2.area.split(" ")[1])) return -1;
     else if(run1.area == run2.area) {
         if(run1.timeSeconds > run2.timeSeconds) return 1;
         else return -1;
