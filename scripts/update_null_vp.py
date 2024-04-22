25e41d74433e35ec91829a462608d8ea8130640d
Kaluub <kaluubs@gmail.com>
2024-04-22 01:46:56 -0400

    Fix script.

--- scripts/update_null_vp.py
+++ scripts/update_null_vp.py
@@ -12,8 +12,11 @@ database = client.Rameses
 bad_accounts = database.accounts.find({"careerVP": None})
 
 for account_record in bad_accounts:
-    api_data = requests.get(f"https://evades.io/api/account/{urllib.parse.quote(account_record['username'])}").json()
-    print(api_data)
+    api_data = requests.get(f"https://evades.io/api/account/{urllib.parse.quote(account_record['username'])}")
+    if not api_data.ok:
+        print(f"FAIL: {account_record}")
+        continue
+    api_data = api_data.json()
     account_record["careerVP"] = api_data["stats"]["highest_area_achieved_counter"]
     print(account_record)
     database.accounts.update_one({"_id": account_record["_id"]}, {"$set": account_record})
