39623e9d2d61eaf1e24d240e4680cfb0dedb0ae5
Kaluub <kaluubs@gmail.com>
2024-08-29 18:52:46 -0400

    Friend list command.

--- interactions/friend.js
+++ interactions/friend.js
@@ -1,5 +1,5 @@
 import DefaultInteraction from "../classes/defaultInteraction.js";
-import { InteractionType, SlashCommandBuilder, SlashCommandStringOption, SlashCommandSubcommandBuilder } from "discord.js";
+import { EmbedBuilder, InteractionType, SlashCommandBuilder, SlashCommandStringOption, SlashCommandSubcommandBuilder } from "discord.js";
 import { DiscordUserData } from "../classes/data.js";
 import Locale from "../classes/locale.js";
 
@@ -32,6 +32,11 @@ class FriendInteraction extends DefaultInteraction {
                         .setRequired(true)
                 )
         )
+        .addSubcommand(
+            new SlashCommandSubcommandBuilder()
+                .setName("list")
+                .setDescription("See a list of all your added friends.")
+        )
 
     constructor() {
         super(FriendInteraction.name, [InteractionType.ApplicationCommand, InteractionType.ApplicationCommandAutocomplete]);
@@ -43,28 +48,67 @@ class FriendInteraction extends DefaultInteraction {
             const response = [];
             for (const username of userData.friends.filter(username => username.includes(interaction.options.getFocused() ?? ""))) {
                 response.push({ name: username, value: username });
-                if (response.length >= 25) break;
+                if (response.length >= 25) {
+                    break;
+                }
             }
             await interaction.respond(response);
         }
         
         if (interaction.isChatInputCommand()) {
             const subcommand = interaction.options.getSubcommand(false);
-            if (subcommand == "add") {
+            if (subcommand === "add") {
                 const userData = await DiscordUserData.getByID(interaction.user.id);
                 const username = interaction.options.getString("username").toLowerCase();
-                if (userData.friends.includes(username)) return Locale.text(interaction, "FRIENDS_ALREADY");
+
+                if (userData.friends.includes(username)) {
+                    return Locale.text(interaction, "FRIENDS_ALREADY");
+                }
+
                 userData.friends.push(username);
                 await userData.save();
+
                 return Locale.text(interaction, "FRIEND_ADDED");
             }
-            if (subcommand == "remove") {
+
+            if (subcommand === "remove") {
                 let userData = await DiscordUserData.getByID(interaction.user.id);
                 const username = interaction.options.getString("friend").toLowerCase();
-                if (!userData.friends.includes(username)) return Locale.text(interaction, "NOT_FRIENDS");
+
+                if (!userData.friends.includes(username)) {
+                    return Locale.text(interaction, "NOT_FRIENDS");
+                }
+
                 userData.friends = userData.friends.filter(friend => friend !== username)
                 await userData.save();
+
                 return Locale.text(interaction, "FRIEND_REMOVED");
+            }
+
+            if (subcommand === "list") {
+                const userData = await DiscordUserData.getByID(interaction.user.id);
+                const sortedFriends = userData.friends.sort((f1, f2) => f1 - f2);
+
+                const embed = new EmbedBuilder()
+                    .setTitle("FRIEND_LIST")
+                    .setColor("#a5b1d3")
+                    .setTimestamp();
+                
+                let description = 'Friends:\n';
+                let addSemicolon = false;
+                for (const friend of sortedFriends) {
+                    let friendText = addSemicolon ? `; ${friend}` : friend;
+                    if (description.length + friendText.length > 1995) {
+                        description += '...';
+                        break;
+                    }
+                    description += friendText;
+                    addSemicolon = true;
+                }
+
+                embed.setDescription(description);
+
+                return { embeds: [embed] }
             }
             return Locale.text(interaction, "HOW_DID_WE_GET_HERE");
         }
